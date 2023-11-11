2cf62727d1ec7f3bf2345aca79edb2da654fde3c
Daniel Ruiz <danielruimar@gmail.com>
2023-11-11 21:19:23 +0100

    h

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,13 +1,31 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import Murcielagos from '@/public/img/murcielagos.svg';
+import Calabaza from '@/public/img/calabaza-smile.svg';
 import HomeHeader from './HomeHeader';
-import HomeContent from './HomeContent';
 
 const Home = () => {
+  const router = useRouter();
+
+  const handleGoToForm = () => {
+    router.push('/form');
+  };
   return (
     <div className='home'>
       <HomeHeader />
-      <HomeContent />
+      <div className='container_container-up'>
+        <h2>No dejes que te asusten...</h2>
+        <Image alt='murcielago' src={Murcielagos} className='container_murcielagos' />
+      </div>
+      <div className='container_container-down'>
+        <h2>Manda un selfie realmente aterrador y participa en el sorteo de un regalo espeluznante!!! </h2>
+      </div>
+      <div className='container_container-button'>
+        <button onClick={handleGoToForm}>PARTICIPA</button>
+        <Image alt='calabaza' src={Calabaza} className='container_calabaza' />
+      </div>
     </div>
   );
 };
